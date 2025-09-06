50311788233938614a0ffd18e2350922e5241a17
Gaurav <gauravkrdas19@gmail.com>
2025-09-06 20:19:53 +0530

    sports category component done

---
+++ src/components/SportsCategories.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+
+const SportsCategories = ({sportsCategory}) => {
+    return (
+        <div className="flex flex-wrap justify-center gap-3 lg:gap-4 w-fit md:w-7/10">
+            {
+                sportsCategory.map((sport, index) => (
+                    <Link
+                        to={`/sport/${sport}`}
+                        key={index}
+                        className='flex flex-col justify-center items-center text-floralWhite font-jaro bg-blackOlive rounded-lg p-3 w-35 text-xl gap-3 lg:gap-4 cursor-pointer hover:bg-blackOlive/80 hover:scale-[1.03] ease-in-out duration-250'
+                    >
+                        <img src={`/Sports Icons/${sport}.svg`} alt="" color="" />
+                        <p className='uppercase'>{sport}</p>
+                    </Link>
+                ))
+            }
+        </div>
+    )
+}
+
+export default SportsCategories
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import SportsCategories from "../components/SportsCategories"
 
 const Home = () => {
   const sportsCategory = [
@@ -8,20 +8,7 @@ const Home = () => {
     <div className='flex flex-col scrollbar justify-center items-center py-10'>
       <p className='font-jaro text-floralWhite text-3xl lg:text-5xl mb-15 lg:mb-20'>Choose your Sports</p>
 
-      <div className="flex flex-wrap justify-center gap-3 lg:gap-4 w-fit md:w-7/10">
-        {
-          sportsCategory.map((sport, index) => (
-            <Link
-              to={`/sport/${sport}`}
-              key={index}
-              className='flex flex-col justify-center items-center text-floralWhite font-jaro bg-blackOlive rounded-lg p-3 w-35 text-xl gap-3 lg:gap-4 cursor-pointer hover:bg-blackOlive/80 hover:scale-[1.03] ease-in-out duration-250'
-            >
-              <img src={`/Sports Icons/${sport}.svg`} alt="" />
-              <p className='uppercase'>{sport}</p>
-            </Link>
-          ))
-        }
-      </div>
+      <SportsCategories sportsCategory={sportsCategory} />
     </div>
   )
 }
